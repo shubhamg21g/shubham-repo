bb1ca3eb6d083e9b2feb277682e740eeabb54bbf
shubham <shubham02749339@gmail.com>
2023-01-20 16:51:52 +0530

    change

--- tomcat.groovy
+++ tomcat.groovy
@@ -19,13 +19,12 @@ pipeline {
         }
         stage('push-artifact') {
             steps {
-                sh ''' sudo apt-get install unzip -y
-                    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-                    unzip awscliv2.zip
-                    sudo ./aws/install
-                    sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
-                    [y]es
-                '''
+                // sh ''' sudo apt-get install unzip -y
+                    // curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+                    // unzip awscliv2.zip
+                    // sudo ./aws/install
+                // '''
+                echo "Installing"
             }
         }
         stage('deploy') {
