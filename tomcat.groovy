daec1a736e24d1803e3136cdf4590f0d1b88202a
shubham <shubham02749339@gmail.com>
2023-01-20 19:46:10 +0530

    change

--- tomcat.groovy
+++ tomcat.groovy
@@ -25,6 +25,7 @@ pipeline {
                     // sudo ./aws/install
                 // '''
                  sh 'sudo mv /home/ubuntu/workspace/jenkins/target/studentapp-2.2-SNAPSHOT.war /home/ubuntu/student-${BUILD_ID}.war'
+                 sh 'aws s3 cp /home/ubuntu/student-${BUILD_ID}.war s3://shubham.goutam --region ap-south-1'
             }
         }
         stage('deploy') {
