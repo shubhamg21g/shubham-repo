849254dc2c32bf85befd208772281c2d350ab128
shubham <shubham02749339@gmail.com>
2023-01-20 14:57:58 +0530

    tomcat.groovy

--- tomcat.groovy
+++ tomcat.groovy
@@ -10,21 +10,21 @@ pipeline {
                 git 'https://github.com/shubhamg21g/student-ui.git'
            }
         }
-        stage(maven-build) {
+        stage('maven-build') {
             steps {
                 sh 'sudo apt-get update -y'
                 sh 'sudo apt-get install maven -y'
                 sh 'mvn clean package'
             }
         }
-        stage(push-artifact) {
+        stage('push-artifact') {
             steps {
                 sh 'wget "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
                     unzip awscliv2.zip
                     sudo ./aws/install'
             }
         }
-        stage(deploy) {
+        stage('deploy') {
             steps {
                 echo 'git deploy'
             }
