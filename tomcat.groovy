1ddb0ef6734ccad84164f30c4db177abc5724481
shubham <shubham02749339@gmail.com>
2023-01-20 14:07:15 +0530

    tomcat.groovy

--- tomcat.groovy
+++ tomcat.groovy
@@ -1,24 +1,25 @@
 pipeline {
     agent {
-        label('ecsnode')
+        label 'ecsnode'
     }
     stages {
         stage('git-pull') {
             steps {
-                sh 'sudo apt install git -y'
-                git credentialsId: 'firstgit', url: 'git@github.com:shubhamg21g/student-ui.git'
+                sh 'sudo apt-get update -y'
+                sh 'sudo apt-get install git -y'
+                git 'https://github.com/shubhamg21g/student-ui.git'
            }
         }
         stage(maven-build) {
             steps {
-                sh 'sudo apt update -y'
-                sh 'sudo apt install maven -y'
+                sh 'sudo apt-get update -y'
+                sh 'sudo apt-get install maven -y'
                 sh 'mvn clean package'
             }
         }
         stage(push-artifact) {
             steps {
-                sh 'curl curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+                sh 'wget "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
                     unzip awscliv2.zip
                     sudo ./aws/install'
             }
