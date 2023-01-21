c68dc50145b59062b6e36aba975fa5aa30e90f19
shubham <shubham02749339@gmail.com>
2023-01-21 18:15:46 +0530

    jenkins-upload

--- tomcat.groovy
+++ tomcat.groovy
@@ -3,34 +3,36 @@ pipeline {
         node ('agent-ec2')
     }
     stages {
-        stage('git-pull') {
+        stage('git-pull'){
             steps {
                 sh 'sudo apt-get update -y'
                 sh 'sudo apt-get install git -y'
                 git 'https://github.com/shubhamg21g/student-ui.git'
            }
         }
-        stage('maven-build') {
+        stage('maven-build'){
             steps {
                 sh 'sudo apt-get update -y'
                 sh 'sudo apt-get install maven -y'
                 sh 'mvn clean package'
             }
         }
-        stage('push-artifact') {
+        stage('push-artifact'){
             steps { 
-             withAWS(credentials: 'shubham', region: 'ap-south-1')
+             withAWS(credentials: 'shubham', region: 'ap-south-1') {
              sh 'aws s3 ls'
-                sh ''' sudo apt-get install unzip -y
-                    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-                    unzip awscliv2.zip
-                    sudo ./aws/install
-                '''
-                 sh 'sudo mv /home/ubuntu/workspace/jenkins/target/studentapp-2.2-SNAPSHOT.war /home/ubuntu/student-${BUILD_ID}.war'
-                 sh 'aws s3 cp /home/ubuntu/student-${BUILD_ID}.war s3://shubham.goutam'
+                sh '''
+                 sudo apt-get install unzip -y
+                # curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+                # unzip awscliv2.zip
+                # sudo ./aws/install
+                 sudo mv /home/ubuntu/workspace/jenkins/target/studentapp-2.2-SNAPSHOT.war /home/ubuntu/student-${BUILD_ID}.war
+                 aws s3 cp /home/ubuntu/student-${BUILD_ID}.war s3://shubham.goutam
+                 '''
+             }
            }
         }
-        stage('tomcat-deploy') {
+        stage('tomcat-deploy'){
             steps {
                 withCredentials([sshUserPrivateKey(credentialsId: 'tomcat-server', keyFileVariable: 'tomcat-server', usernameVariable: 'tomcat')]) {
                     sh'''
