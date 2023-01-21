7bf0d806af65036985f35734aa3ac08c4843dce0
shubham <shubham02749339@gmail.com>
2023-01-21 15:34:52 +0530

    jenkins-upload

--- tomcat.groovy
+++ tomcat.groovy
@@ -32,24 +32,11 @@ pipeline {
         }
         stage('tomcat-deploy') {
             steps {
-                // withCredentials([sshUserPrivateKey(credentialsId: 'tomcat-server', keyFileVariable: 'tomcat-server', usernameVariable: 'tomcat')]) {
+                withCredentials([sshUserPrivateKey(credentialsId: 'tomcat-server', keyFileVariable: 'tomcat-server', usernameVariable: 'tomcat')]) {
                     sh'''
                         ssh -i ${tomcat-server} -o StrictHostKeyChecking=no ubuntu@43.205.143.41<<EOF
-                        sudo apt-get install unzip -y
-                        sudo apt-get install openjdk-11-jre -y
-                        sudo apt-get update -y
-                        curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-                        unzip awscliv2.zip
-                        sudo ./aws/install
-                        aws s3 cp s3://shubham.goutam/student-${BUILD_ID}.war .
-                        curl -O https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.85/bin/apache-tomcat-8.5.85.tar.gz
-                        sudo tar -xvf apache-tomcat-8.5.85.tar.gz -C /opt/
-                        sudo sh /opt/apache-tomcat-8.5.85/bin/shutdown.sh
-                        sudo cp -rv student-${BUILD_ID}.war studentapp.war
-                        sudo cp -rv studentapp.war /opt/apache-tomcat-8.5.85/webapps/
-                        sudo sh /opt/apache-tomcat-8.5.85/bin/startup.sh
                     '''
-                // }
+                }
             }
         }
      }            
