a4af1c1bff5eea6f476ee678797de77006893322
shubham <shubham02749339@gmail.com>
2023-01-28 16:59:32 +0530

    parameter

--- tomcat.groovy
+++ tomcat.groovy
@@ -45,7 +45,7 @@ pipeline {
                         #curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
                         #unzip awscliv2.zip
                         #sudo ./aws/install
-                        aws s3 cp s3://shubham.goutam/student-${BUILD_ID}.war .
+                        aws s3 cp s3://shubham.goutam/student-${verion}.war .
                         curl -O https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.85/bin/apache-tomcat-8.5.85.tar.gz
                         sudo tar -xvf apache-tomcat-8.5.85.tar.gz -C /opt/
                         sudo sh /opt/apache-tomcat-8.5.85/bin/shutdown.sh
